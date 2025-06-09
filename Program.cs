faeb64433f70f0075e574c752c58b05ca6e45001
ColumbidaeKalla <sunilkalla4444@gmail.com>
2025-06-09 20:20:53 +0545

    DotNet Run() Method to run all course programs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DotNetCourse;
+
+using System;
+using src.Constant;
+
+using src.SessionFour.AppDelegate;
+
+internal static class Program {
+    private static void Main(string[] args) {
+        if (args.Length > 0)
+        {
+            string combinedArgs = string.Join(", ", args);
+            // Console.WriteLine("Here are the args: " + combinedArgs);
+        }
+        else
+        {
+            // Console.WriteLine($"{AppString.RunWithoutArgument}");
+            // Console.WriteLine(AppString.Separator);
+
+            /* 
+            RunAppPoly.RunCustomStack();
+
+            AppTryCatch.ZeroDivision();
+            Link.GroupNames();
+            RunAppLinq.LinqJoin(); 
+            */
+
+            RunAppDelegate.RunSimpleDelegate();
+            // Console.WriteLine(AppString.Separator);
+        }
+    }
+}
