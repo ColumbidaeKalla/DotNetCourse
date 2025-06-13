4da3f4cf7f4d08919d35e0b6e19f9a602268d018
ColumbidaeKalla <sunilkalla4444@gmail.com>
2025-06-13 19:29:01 +0545

    Programs Execution with Run Method

--- Program.cs
+++ Program.cs
@@ -1,32 +1,32 @@
-﻿namespace DotNetCourse;
+﻿// namespace DotNetCourse;
 
-using System;
-using src.Constant;
+// using System;
+// using src.Constant;
 
-using src.SessionFour.AppDelegate;
+// using src.SessionFour.AppDelegate;
 
-internal static class Program {
-    private static void Main(string[] args) {
-        if (args.Length > 0)
-        {
-            string combinedArgs = string.Join(", ", args);
-            // Console.WriteLine("Here are the args: " + combinedArgs);
-        }
-        else
-        {
-            // Console.WriteLine($"{AppString.RunWithoutArgument}");
-            // Console.WriteLine(AppString.Separator);
+// internal static class Program {
+//     private static void Main(string[] args) {
+//         if (args.Length > 0)
+//         {
+//             string combinedArgs = string.Join(", ", args);
+//             // Console.WriteLine("Here are the args: " + combinedArgs);
+//         }
+//         else
+//         {
+//             // Console.WriteLine($"{AppString.RunWithoutArgument}");
+//             // Console.WriteLine(AppString.Separator);
 
-            /* 
-            RunAppPoly.RunCustomStack();
+//             /* 
+//             RunAppPoly.RunCustomStack();
 
-            AppTryCatch.ZeroDivision();
-            Link.GroupNames();
-            RunAppLinq.LinqJoin(); 
-            */
+//             AppTryCatch.ZeroDivision();
+//             Link.GroupNames();
+//             RunAppLinq.LinqJoin(); 
+//             */
 
-            RunAppDelegate.RunSimpleDelegate();
-            // Console.WriteLine(AppString.Separator);
-        }
-    }
-}
+//             RunAppDelegate.RunSimpleDelegate();
+//             // Console.WriteLine(AppString.Separator);
+//         }
+//     }
+// }
