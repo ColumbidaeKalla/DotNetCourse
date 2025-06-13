084c22ad3a6434d98d0da5290d00b2a2c284501b
ColumbidaeKalla <sunilkalla4444@gmail.com>
2025-06-13 19:29:44 +0545

    Exercise-Unit-3 (Class and Object)

---
+++ Exercises/Unit-3/ClassObject.cs
@@ -0,0 +1,32 @@
+/* namespace DotNetCourse.Exercises.Unit3;
+
+using System;
+
+class Person
+{
+    private string? Name;
+    private int Age;
+    private string? Gender;
+
+    public void SetInfo(string name, int age, string gender)
+    {
+        Name = name;
+        Age = age;
+        Gender = gender;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}, Gender: {Gender}");
+    }
+}
+
+class ClassObject
+{
+    public static void Main()
+    {
+        Person person1 = new Person();
+        person1.SetInfo("Sunil Shrestha", 21, "Male");
+        person1.Display();
+    }
+} */
